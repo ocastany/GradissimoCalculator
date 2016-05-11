91181092efe547b324e03b3bfeff09e3b1e6a6d0
Olivier Castany <olivier.castany@telecom-bretagne.eu>
2016-05-11 22:44:54 +0200

    Improve test exemple T4

--- examples/T4_Plots.py
+++ examples/T4_Plots.py
@@ -3,25 +3,35 @@
 
 from gradissimo import *
 
-# Study of Gradissimo fibers.
-# Parameters of the GI fiber are taken from table 4 in this article:
-# M. Thual, Opt. Eng., 46(1), p. 015402 (2007)
+# Study of Gradissimo fibers and comparison with reference articles
+# [1] P. Chanclou, JLT, 17(5), p. 924 (1999)
+# [2] M. Thual, Opt. Eng., 46(1), p. 015402 (2007)
+
+# Value of the spatial pulsation
+# GRIN 85/125 µm : 4294 m⁻¹ @ 1.3 µm    [1]
+#                  4117 m⁻¹ @ 1.55 µm   [2]
+
+# Refractive index of silica
+# n = 1.447 @ 1.3 µm                    [1]
+#     1.469 @ 1.55 µm
+
+# We will reproduce figures 5 and 6 from [1]
 
 # Define the wavelength and the materials
-set_wavelength(1.55e-6)
-n_fiber = 1.44
-n_out = 1.00    
-input_fiber = SingleModeFiber(w=5.2e-6, n=n_fiber)
+set_wavelength(1.3e-6)
+n_fiber = 1.447
+n_out = 1.000
+input_fiber = SingleModeFiber(w=5.0e-6, n=n_fiber)
 HS = HomogeneousSpace(n_fiber)
-GI = GradientIndexFiber(n_fiber, gamma=4117, diam=85e-6)
+GI = GradientIndexFiber(n_fiber, gamma=4294, diam=85e-6)
 OUT = HomogeneousSpace(n_out)
 
 # Gradissimo fiber structure...
 G = Gradissimo(input_fiber, HS, GI, OUT)
 
 # Segment lengths to be used
-L_HS_list = [0, 200, 400]
-L_GI_list = linspace(0, 800, num=100)
+L_HS_list = [0, 200, 400, 600]
+L_GI_list = linspace(0, 800, num=200)
 
 data_list = []
 for (i, L_HS) in enumerate(L_HS_list):
@@ -36,7 +46,7 @@ data = numpy.array(data_list)
 
 # Trace the plots
 pyplot.figure()
-pyplot.title("Waist diameter versus GRIN length")
+pyplot.title("Waist diameter for various silica length (L_HS)")
 for (i, L_HS) in enumerate(L_HS_list):
     pyplot.plot(L_GI_list, data[i,:,0], label="L_HS = {:} µm".format(L_HS))
 pyplot.xlabel("Gradient index segment length (GI 85/125) (µm)")
@@ -44,12 +54,13 @@ pyplot.ylabel("Waist diameter (µm)")
 pyplot.legend()
 
 pyplot.figure()
-pyplot.title("Working distance versus GRIN length")
+pyplot.title("Working distance for various silica length (L_HS)")
 for (i, L_HS) in enumerate(L_HS_list):
     pyplot.plot(L_GI_list, data[i,:,1], label="L_HS = {:} µm".format(L_HS))
 pyplot.xlabel("Gradient index segment length (GI 85/125) (µm)")
 pyplot.axhline(color="k")
 pyplot.ylabel("Working distance (µm)")
+pyplot.ylim(ymin=0)
 pyplot.legend()
 
 pyplot.show()
